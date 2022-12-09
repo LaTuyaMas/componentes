69ca8386f9e5ea3712bc4c3c6d02c8db0fdb75d9
LaTuyaMas <miguel.cano.roman@cieep.com>
2022-12-09 12:25:37 +0100

    avatares

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'grid-buttons',
     loadChildren: () => import('./pages/grid-buttons/grid-buttons.module').then( m => m.GridButtonsPageModule)
   },
+  {
+    path: 'avatar',
+    loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
+  },
 ];
 
 @NgModule({
